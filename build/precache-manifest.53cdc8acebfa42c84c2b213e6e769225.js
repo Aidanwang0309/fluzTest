9f06ffda0cde33895d92e21ea328cf3e1efe009a
Aidanwang0309 <aidanwang0309@gmail.com>
2019-03-09 19:59:56 -0500

    online test

---
+++ build/precache-manifest.53cdc8acebfa42c84c2b213e6e769225.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca7e6e63aa8bc0fd44fc",
+    "url": "/static/css/main.0c0dda15.chunk.css"
+  },
+  {
+    "revision": "ca7e6e63aa8bc0fd44fc",
+    "url": "/static/js/main.9f67159f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "6333a275b3c73024c6e5",
+    "url": "/static/js/2.7c68cb95.chunk.js"
+  },
+  {
+    "revision": "1859cb1c663512646ecde6d3e8e86350",
+    "url": "/static/media/dollar-sign.1859cb1c.svg"
+  },
+  {
+    "revision": "1f7bbb476d441ac539bdc40a7f298c24",
+    "url": "/static/media/apple-itunes.1f7bbb47.svg"
+  },
+  {
+    "revision": "341c2ce9d0f0404676603cb9f2da5cae",
+    "url": "/static/media/itunes.341c2ce9.jpg"
+  },
+  {
+    "revision": "558cac3cada2a9975462715df09e563e",
+    "url": "/static/media/nike.558cac3c.svg"
+  },
+  {
+    "revision": "b965b1e3d15d87d6416a9343a9f56835",
+    "url": "/static/media/nike.b965b1e3.jpg"
+  },
+  {
+    "revision": "ec9208097d7c06bb494390efde1649fc",
+    "url": "/static/media/panera-bread.ec920809.svg"
+  },
+  {
+    "revision": "b870a7ddc4699bf33f855059144d7561",
+    "url": "/static/media/panera.b870a7dd.jpg"
+  },
+  {
+    "revision": "1ff35b0321cf3a642828d753ad44692f",
+    "url": "/static/media/starbucks.1ff35b03.svg"
+  },
+  {
+    "revision": "028c9ee11f66e081ec9b7793147fc287",
+    "url": "/static/media/starbucks.028c9ee1.jpg"
+  },
+  {
+    "revision": "6892d040faf7e7c0a93ad94765a3d733",
+    "url": "/index.html"
+  }
+];
